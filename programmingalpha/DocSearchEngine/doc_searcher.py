8c917da4a0443215fe142268eebc4b75a3392760
Myth <caipengbo@outlook.com>
2019-07-16 16:34:06 +0800

    Remove json escape character

--- programmingalpha/DocSearchEngine/doc_searcher.py
+++ programmingalpha/DocSearchEngine/doc_searcher.py
@@ -20,7 +20,19 @@ class DocSearcherHTTPProxy(AlphaHTTPProxy):
         query.arrange()
         post_results = query.get_results()
 
-        post_json_list = json.dumps(post_results, cls=PostJSONEncoder2)
+        # post_json_list = json.dumps(post_results, cls=PostJSONEncoder2)
+       
+        post_json_list = []
+        for obj in post_results:
+            all_text = obj.question_obj.title + ' ' + obj.question_obj.parsed_body
+            for i, ans_obj in enumerate(obj.answer_obj_list):
+                if i == 3:
+                    break
+                all_text = all_text + ' ' + ans_obj.parsed_body
+
+            dic = {'id': obj.question_obj.es_id, 'text': all_text}
+            post_json_list.append(dic)
+        
         results = {}
         results['posts'] = post_json_list
         results['question'] = title
