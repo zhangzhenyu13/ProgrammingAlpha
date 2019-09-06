9e4d43c5a27adfb2a37075bce8be1939cbcbc351
Myth <caipengbo@outlook.com>
2019-09-06 09:39:32 +0800

    Implements doc searcher by tornado http server

--- programmingalpha/DocSearchEngine2/doc_searcher.py
+++ programmingalpha/DocSearchEngine2/doc_searcher.py
@@ -1,26 +1,37 @@
-from programmingalpha.alphaservices.HTTPServers.flask_http4doc_searcher import AlphaHTTPProxy4DocSearcher
+from programmingalpha.alphaservices.HTTPServers.tornado_http import AlphaHTTPProxy
 import json
 
 from programmingalpha.DocSearchEngine2.entity.query import Query
 from programmingalpha.Utility import getLogger
 logger=getLogger(__name__)
 
-class DocSearcherHTTPProxy(AlphaHTTPProxy4DocSearcher):
+class DocSearcherHTTPProxy(AlphaHTTPProxy):
     def __init__(self, config_file):
         super().__init__(config_file)
         logger.info("{} server initing".format(self.args.ServiceName))
 
     def processCore(self, data):
-        print("*************************************begin search1")
-        title = data["Title"]
-        body = data["Body"]
-        tag_list = data["Tags"]
-        size = data["size"]
+        title = ''
+        body = ''
+        tag_list = ''
+
+        size = self.args.doc_size
+        if 'Title' in data:
+            title = data.get('Title')
+        if 'Body' in data:
+            body = data.get('Body')
+        if 'Tags' in data:
+            tag_list = data.get('Tags')
+        if 'size' in data:
+            size = data.get('size')
+
+        if title == '' and body == '':
+            logger.error("Parameters Error: You must <b>Title</b> and <b>Body</b> parameters!")
+            return
+
         query = Query(title=title, body=body, tag_list=tag_list, num_works=self.args.num_works)
-        print("*************************************begin search2")
-        query.search(size=size)
+        query.search(url=self.args.es_url, size=size)
         query.arrange()
         results = query.get_origin_results()
 
-        print("-------", len(results), type(results))
         return results
